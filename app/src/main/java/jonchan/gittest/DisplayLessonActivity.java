191afb75abb492ce5b45096252b4989ace6c4a24
cwnwayne <32767391+cwnwayne@users.noreply.github.com>
2017-11-17 13:20:34 +0800

    move lesson inside student

--- app/src/main/java/jonchan/gittest/DisplayLessonActivity.java
+++ app/src/main/java/jonchan/gittest/DisplayLessonActivity.java
@@ -1,8 +1,10 @@
 package jonchan.gittest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +14,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,38 +37,31 @@ public class DisplayLessonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displaylesson);
 
+        Intent myIntent = getIntent();
+        final String student_uid = myIntent.getStringExtra("STUDENT_ID");
+
         mListView = (ListView) findViewById(R.id.listview);
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mLessonDetail);
         mListView.setAdapter(arrayAdapter);
 
         mAuth = FirebaseAuth.getInstance();
-        String user_id = mAuth.getCurrentUser().getUid();
+        final String user_id = mAuth.getCurrentUser().getUid();
         database = FirebaseDatabase.getInstance();
-        mRef = database.getReference("Users").child(user_id).child("Lesson");
+        mRef = database.getReference("Lesson");
 
-        mRef.addChildEventListener(new ChildEventListener() {
+        mRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                    Log.d("Teacher", postSnapshot.child("Teacher").getValue().toString());
+                    Log.d("Student", postSnapshot.child("Student").getValue().toString());
+                    if((postSnapshot.child("Teacher").getValue().toString()).equals(user_id) && (postSnapshot.child("Student").getValue().toString()).equals(student_uid)){
 
-                String value = dataSnapshot.getValue(String.class);
-                mLessonDetail.add(value);
-                arrayAdapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+                        String value = postSnapshot.child("Date").getValue(String.class);
+                        mLessonDetail.add(value);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
             }
 
             @Override
@@ -72,8 +69,6 @@ public class DisplayLessonActivity extends AppCompatActivity {
 
             }
         });
-
-
 
 
     }
