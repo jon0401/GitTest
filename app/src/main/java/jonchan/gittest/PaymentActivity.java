cd4a0ce230dce2414325cd71ca0d7b29763b63de
cwnwayne <32767391+cwnwayne@users.noreply.github.com>
2017-11-23 22:32:16 +0800

    fix some bugs

--- app/src/main/java/jonchan/gittest/PaymentActivity.java
+++ app/src/main/java/jonchan/gittest/PaymentActivity.java
@@ -1,22 +1,40 @@
 package jonchan.gittest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class PaymentActivity extends Activity {
 
     private ImageView pament_back;
+    private Button btnPayNow;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
         pament_back=(ImageView)findViewById(R.id.pament_back);
+        btnPayNow = (Button) findViewById(R.id.btnPayNow);
+
         pament_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btnPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                try{
+                    startActivity(myIntent);
+                }catch(android.content.ActivityNotFoundException e){
+                    e.printStackTrace();
+                }
             }
         });
     }
