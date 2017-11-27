963874bd5916227ae4868ee8c6cf2baac0c8fdbb
cwnwayne <32767391+cwnwayne@users.noreply.github.com>
2017-11-27 21:53:45 +0800

    student homepage add contact

--- app/src/main/java/jonchan/gittest/MainActivity.java
+++ app/src/main/java/jonchan/gittest/MainActivity.java
@@ -257,7 +257,7 @@ public class MainActivity extends AppCompatActivity {
                                         } else if (itemId == R.id.navigation_game) {
                                             startActivity(new Intent(MainActivity.this, jonchan.musex.MainActivity.class));
                                         } else if (itemId == R.id.navigation_contact){
-                                            startActivity(new Intent(MainActivity.this, DisplayStudentActivity.class));
+                                            startActivity(new Intent(MainActivity.this, DisplayTeacherActivity.class));
                                         }
                                         finish();
                                     }, 300);
